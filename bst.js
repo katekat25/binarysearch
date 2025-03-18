9c5891f4c48039947250f17ff57b264df8612ffa
Kate Schumacher <kate.schumacher@gmail.com>
2025-03-18 12:05:13 -0500

    Minor fixes

--- bst.js
+++ bst.js
@@ -47,3 +47,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
 };
+
+let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+console.log(sort(testArray));
+let tree = new Tree(testArray);
+prettyPrint(tree.root);
